f15f533a7d6a61665ed5dfa245f3cd812b208f9d
ParkerCF <faul.parker@outlook.com>
2022-12-17 13:20:41 -0500

    adding more JS code

--- script.js
+++ script.js
@@ -71,11 +71,51 @@ function displayWeather(city, weather) {
     icon.setAttribute('alt', weatherDescrip);
     icon.setAttribute('class', 'weather-img');
     heading.append(icon);
-    tempEl.textContent = `Temp: ${faren}°F`;
+    tempEl.textContent = `Temp: ${faren} °F`;
     windEl.textContent = `Wind: ${windSpeed} MPH`;
     humidityEl.textContent = `Humidty: ${humidity} %`;
     body.append(heading, tempEl, windEl, humidityEl);
 
     todayContainer.innerHTML = '';
     todayContainer.append(card);
+}
+
+
+function dispayForecast(forecast) {
+    var weatherURL = `https://openweathermap.org/img/w/${forecase.weather[0].icon}.png`;
+    var weatherDescrip = forecast.weather[0].description;
+    var faren = forecast.main.temp;
+    var humidity = forecast.main.humidity;
+    var windSpeed = forecast.wind.speed;
+
+    var col = document.createElement('div');
+    var card = document.createElement('div');
+    var body = document.createElement('div');
+    var title = document.createElement('h5');
+    var icon = document.createElement('img');
+    var tempEl = document.createElement('p');
+    var windEl = document.createElement('p');
+    var humidityEl = document.createElement('p');
+
+    col.append(card);
+    card.append(body);
+    body.append(title, icon, tempEl, windEl, humidityEl);
+
+    col.setAttribute('class', 'col-md');
+    col.classList.add('five-day-card');
+    card.setAttribute('class', 'card bg-primary h-100 text-white');
+    body.setAttribute('class', 'card-body p-2');
+    title.setAttribute('class', 'card-title');
+    tempEl.setAttribute('class', 'card-text');
+    windEl.setAttribute('class', 'card-text');
+    humidityEl.setAttribute('class', 'card-text');
+
+    title.textContent = day.js(forecast.dt_txt).format('M/D/YYYY');
+    icon.setAttribute('src', weatherURL);
+    icon.setAttribute('alt', weatherDescrip);
+    tempEl.textContent = `Temp: ${faren} °F`;
+    windEl.textContent = `Wind: ${windSpeed} MPH`;
+    humidityEl.textContent = `Humidity: ${humidity} %`;
+
+    forecastContainer.append(col);
 }
